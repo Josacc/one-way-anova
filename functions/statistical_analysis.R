cb3132216612b455e2735e330466adbf7bcb905a
Josacc <77957363+Josacc@users.noreply.github.com>
2025-08-14 00:49:56 -0600

    Updates

--- functions/statistical_analysis.R
+++ functions/statistical_analysis.R
@@ -1,26 +1,40 @@
-shapiro <- function(d, n_col){
-  d[[1]] %>%
+shapiro_test <- function(data, n_col){
+  st <- data[[1]] %>%
     unique() %>%
-    sapply(function(x)(d %>% filter(d[[1]] == x))[[n_col]] %>% shapiro.test())
+    sapply(function(x)(data %>% filter(data[[1]] == x))[[n_col]] %>% shapiro.test())
+
+  st <- as.data.frame(st) %>% .[c(1, 2), ]
+  return(st)
 }
 
-bar <- function(d, n_col){
-  bartlett.test(d[[n_col]] ~ d[[1]])
+bartlett_test <- function(data, n_col){
+  bt <- bartlett.test(data[[n_col]] ~ data[[1]])
+  bt <- tibble(`Bartlett's K-squared` = bt$statistic, `p-value` = bt$p.value)
+  return(bt)
 }
 
-an <- function(d, n_col){
-  # print("ANOVA")
-  # aov(d[[n_col]] ~ d[[1]]) %>%
-  #   summary()
-
-  anova_res <- aov(d[[n_col]] ~ d[[1]], data = d)
-  return(summary(anova_res)[[1]])
+anova_test <- function(data, x = 'Treatments',  n_col){
+  at <- aov(data[[n_col]] ~ data[[1]], data = data)
+  at <- summary(at)[[1]] %>%
+    `row.names<-`(c(x, 'Residuals'))
+  return(at)
 }
 
-analisis <- function(d, n_col){
-
-  print(shapiro(d , n_col))
-  print(bar(d , n_col))
-  print(an(d , n_col))
-
+ui <- fluidPage(
+  tableOutput('table_anova'),
+  tableOutput('table_bar'),
+  tableOutput('table_shapiro')
+)
+server <- function(input, output, session) {
+  output$table_anova <- renderTable({
+    anova_test(d, n_col = 2)
+  }, rownames = TRUE, na = "" )
+  output$table_bar <- renderTable({
+    bartlett_test(d, 2)
+  })
+  output$table_shapiro <- renderTable({
+    shapiro_test(d, 2)
+  }, rownames = TRUE, digits = 4, na = "" )
 }
+
+shinyApp(ui, server)
